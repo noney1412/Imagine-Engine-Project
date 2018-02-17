92a157a0d82584fe4bca191843708e5e5fb12ac9
noney1412 <nonpanpila@gmail.com>
2018-02-17 14:49:40 +0700

    :wrench: fixed

---
+++ build/.next/bundles/pages/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([4],{207:function(e,t,n){e.exports=n(208)},208:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(6),r=function(e){return e&&e.__esModule?e:{default:e}}(u),l="D:\\Project\\GitHub\\Imagine-Engine-Project\\client\\pages\\index.js?entry";t.default=function(){return r.default.createElement("div",{__source:{fileName:l,lineNumber:2}},r.default.createElement("h1",{__source:{fileName:l,lineNumber:3}},"Hello"),r.default.createElement("h1",{__source:{fileName:l,lineNumber:4}},"LOLL"))}}},[207]);
+            return { page: comp.default }
+          })
+        
